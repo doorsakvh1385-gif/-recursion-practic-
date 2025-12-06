bbdc3d9d5d86dcc296bfe66cf0a3ce4b8e2c0e9c
Dor <doorsakvh1385@gmail.com>
2025-12-06 11:13:16 +0330

    Add files via upload

---
+++ fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("fibonachi " + n + ":");
+            PF(0, 1, n);
+            Console.ReadKey();
+        }
+      
+        static void PF(int a, int b, int max)
+        {
+          
+            if (a > max)
+                return;
+
+            Console.Write(a + " , ");
+           
+            PF(b, a + b, max);
+        }
+    }
+}
