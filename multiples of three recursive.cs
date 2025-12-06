36c527454b08abd50a17d33ad9a73915d1c9b2e6
Dor <doorsakvh1385@gmail.com>
2025-12-06 10:44:36 +0330

    Add files via upload

---
+++ multiples of three recursive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter one number ");
+            int n = int.Parse(Console.ReadLine());
+            PMO3(n - 1);
+        }
+        static void PMO3(int n) { if (n <= 0)
+                return;
+            if (n % 3 == 0)
+                Console.WriteLine(n);
+            PMO3(n - 1);
+        }
+        
+        
+           
+    }
+}
